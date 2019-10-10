5f01075316d00498a4447b1d806603834b4616d1
jusalanen <jussialanen9@gmail.com>
2019-10-10 21:46:54 +0300

    lisää tehtävän 5.3 logged user create blog

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import loginService from './services/login'
 import Notification from './components/Notification'
 import Blog from './components/Blog'
 import blogService from './services/blogs'
+import LoginForm from './components/LoginForm'
+import BlogForm from './components/BlogForm'
 
 const App = () => {
   const [ blogs, setBlogs ] = useState([]) 
@@ -10,7 +12,9 @@ const App = () => {
   const [ password, setPassword ] = useState('')
   const [user, setUser] = useState(null)
   const [ message, setMessage ] = useState(null)
-
+  const [ newTitle, setNewtitle ] = useState('')
+  const [ newAuthor, setNewauthor ] = useState('')
+  const [ newUrl, setNewurl ] = useState('')
 
   const hook = () => {
     console.log('effect')
@@ -69,32 +73,32 @@ const App = () => {
     setMessage(null)
   }
 
+  const addBlog = async (event) => {
+    event.preventDefault()
+    const blogObject = {
+      title: newTitle,
+      author: newAuthor,
+      url: newUrl
+    }
+    const saveBlog = await blogService.create(blogObject)
+    setBlogs(blogs.concat(saveBlog))
+
+    setNewtitle('')
+    setNewauthor('')
+    setNewurl('')
+  }
+
   if (user === null) {
     return (
       <div>
         <h2>log in to application</h2>
         <Notification message={message} />
 
-        <form onSubmit={handleLogin}>
-          <div>
-            username <input
-              type="text"
-              value={username}
-              name="Username"
-              onChange={({ target }) => setUsername(target.value)}
-            />
-          </div>
-          <div>
-            password <input
-              type="password"
-              value={password}
-              name="Password"
-              onChange={({ target }) => setPassword(target.value)}
-            />
-          </div>
-          <button type="submit">login</button>
-        </form>
-  
+        <LoginForm handleLogin = {handleLogin}
+                username = {username}
+                setUsername = {setUsername}
+                password = {password}
+                setPassword = {setPassword} />  
       </div>
     )
   }
@@ -102,10 +106,16 @@ const App = () => {
   return (
     <div>
       <h2>Blogs</h2>
-
       <table><tbody><tr><td width='200'><Notification message={message} /></td>
       <td width='50'><button onClick = { () => {
         logOut()}}>logout</button></td></tr></tbody></table>
+      <BlogForm addBlog = {addBlog}
+                title = {newTitle}
+                setTitle = {setNewtitle}
+                author = {newAuthor}
+                setAuthor = {setNewauthor}
+                url = {newUrl}
+                setUrl = {setNewurl} /><br></br>
       {blogs.map(blog =>
         <Blog key={blog.id} blog={blog} />
       )}
---
+++ src/components/BlogForm.js
@@ -0,0 +1,46 @@
+import React from 'react'
+
+const BlogForm = props => {
+  const addBlog = props.addBlog
+  const title = props.title
+  const setTitle = props.setTitle
+  const author = props.author
+  const setAuthor = props.setAuthor
+  const url = props.url
+  const setUrl = props.setUrl
+
+  return (
+    <div>
+      <h3>Add new blog</h3>
+      <form onSubmit={addBlog}>
+          <div>
+            title <input
+              type="text"
+              value={title}
+              name="Title"
+              onChange={({ target }) => setTitle(target.value)}
+            />
+          </div>
+          <div>
+            author <input
+              type="author"
+              value={author}
+              name="Author"
+              onChange={({ target }) => setAuthor(target.value)}
+            />
+          </div>
+          <div>
+            url <input
+              type="url"
+              value={url}
+              name="Url"
+              onChange={({ target }) => setUrl(target.value)}
+            />
+          </div>
+          <button type="submit">add blog</button>
+      </form>
+    </div>
+  )
+}
+
+export default BlogForm
