05fd9c399f920a97ecc918ef8a205508bed81196
jusalanen <jussialanen9@gmail.com>
2019-10-17 20:32:32 +0300

    lisää tehtävän 5.14 button click test

--- src/components/SimpleBlog.test.js
+++ src/components/SimpleBlog.test.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '@testing-library/jest-dom/extend-expect'
-import { render, cleanup } from '@testing-library/react'
+import { render, cleanup, fireEvent } from '@testing-library/react'
 import SimpleBlog from './SimpleBlog'
 
 afterEach(cleanup)
@@ -22,4 +22,24 @@ test('renders title, author and likes', () => {
   expect(component.container).toHaveTextContent(
     'blog has 999 likes'
   )
+})
+
+test('clicking button twice calls event handler 2 times', () => {
+  const testBlog = {
+    title: 'First test blog',
+    author: 'Tester One',
+    likes: 999
+  }
+
+  const handler =  jest.fn()
+
+  const { getByText } = render(
+    <SimpleBlog blog={testBlog} onClick={handler}/>
+  ) 
+
+  const button = getByText('like')
+  fireEvent.click(button)
+  fireEvent.click(button)
+
+  expect(handler.mock.calls.length).toBe(2)
 })
